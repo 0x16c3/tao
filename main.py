51437a821c66ea1f48e799af87df020ed37111e3
iinfin <han@iinf.in>
2020-07-16 17:31:09 +0300

    fix url

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ async def on_message(message):
     # embed meta
     embed = discord.Embed(color=0xF5F5F5)
 
-    source = "https://github.com/someone/tao"
+    source = "https://github.com/iinfin/tao"
     avatar = client.user.avatar_url
 
     if message.content == "tao":
