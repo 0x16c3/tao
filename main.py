f893a2d2d0d5b5d03f805cdc1c612d97436446dd
emir öncel <emirhan@tuta.io>
2020-07-19 11:00:39 +0300

    update send_score_info

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ async def on_message(message):
 
                 await Score.sort_user_auto(Score, channel_notify, message.author, True)
                 if verbose:
-                    await Score.send_score_info(Score, message.channel, message.author, True, True)
+                    await Score.send_score_info(Score, channel_notify, message.author, True, True)
 
                 # update file
                 with open("cogs/_user.json", "r") as f:
