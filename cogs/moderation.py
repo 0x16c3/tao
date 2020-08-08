10a51d23f3d366237a9863ddf7ee011b852fc77f
0x16c3 <emirhan@tuta.io>
2020-08-08 21:08:44 +0300

    update moderation time string

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -69,16 +69,16 @@ class Moderation(commands.Cog):
                         duration_str = "`a week`"
                     else:
                         duration_str = "`" + time_str[:-1] + " weeks`"
-                elif (
-                    not args_first[-1] == "w"
-                    or args_first[-1] == "week"
-                    and args_first[-1] == "d"
-                    or args_first[-1] == "day"
-                    and args_first[-1] == "h"
-                    or args_first[-1] == "hour"
-                    and args_first[-1] == "m"
-                    or args_first[-1] == "minute"
-                ):
+                elif
+                    (not args_first[-1] == "w"
+                    or not args_first[-1] == "week")
+                    and (not args_first[-1] == "d"
+                    or not args_first[-1] == "day")
+                    and (not args_first[-1] == "h"
+                    or not args_first[-1] == "hour")
+                    and (not args_first[-1] == "m"
+                    or not args_first[-1] == "minute")
+                :
                     embed_errr = discord.Embed(
                         title="Error", description="", color=color_errr
                     )
